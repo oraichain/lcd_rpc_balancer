f05f4256594322c1070a6a9fadab119e8d9427c7
phutx <phu.tx@orai.io>
2024-07-10 17:29:20 +0700

    add more network

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM openresty/openresty:alpine
 
 WORKDIR /workspace
 
-RUN apk update && apk add --no-cache bash busybox-suid
+RUN apk update && apk add --no-cache bash busybox-suid goaccess curl net-tools
 
 RUN apk add perl curl && opm get ledgetech/lua-resty-http bungle/lua-resty-template
 
@@ -20,7 +20,9 @@ RUN chmod +x /etc/nginx/conf.d/job.sh
 RUN echo "* * * * * /etc/nginx/conf.d/job.sh >> /var/log/cron.log 2>&1" > /etc/crontabs/root
 
 # Create the log file to be able to run tail
-RUN touch /var/log/cron.log
+RUN touch /var/log/cron.log /var/log/access.log
+
+RUN goaccess /var/log/access.log -o /var/www/templates/index.html --real-time-html --ws-url=ws://3.134.19.98:80/_arws/  --date-format='%d/%b/%Y' --time-format='%T' --log-format=COMBINED --daemonize
 
 # Ensure nginx runs in the foreground and start cron in the background
 CMD crond && nginx -g 'daemon off;'
--- conf.d/job.sh
+++ conf.d/job.sh
@@ -14,8 +14,17 @@ IPS_OSMO=$(curl -s 'https://devops-tool.orai.io/node_api.php?network=osmosis-1')
 # cosmoshub-4
 IPS_ATOM=$(curl -s 'https://devops-tool.orai.io/node_api.php?network=cosmoshub-4')
 
-# injecttive-1
+# injective-1
 IPS_INJ=$(curl -s 'https://devops-tool.orai.io/node_api.php?network=injective-1')
+
+# injective-1
+IPS_TIA=$(curl -s 'https://devops-tool.orai.io/node_api.php?network=celestia')
+
+# injective-1
+IPS_DYM=$(curl -s 'https://devops-tool.orai.io/node_api.php?network=dymension_1100-1')
+
+# injective-1
+IPS_NOBLE=$(curl -s 'https://devops-tool.orai.io/node_api.php?network=noble-1')
 
 if [ -n "$IPS_OSMO" ]; then
     sed -i.bak -E "s|^([[:space:]]+local[[:space:]]+osmo_up[[:space:]]+=[[:space:]]+resty_roundrobin:new).*$|\1\(\{"$IPS_OSMO"\}\)|" /etc/nginx/conf.d/default.conf
@@ -35,4 +44,21 @@ else
     curl -s -H "Content-Type: application/json" -X POST -d "$(generate_post_data "The string INJ is empty")" $discord_url
 fi
 
+if [ -n "$IPS_TIA" ]; then
+    sed -i.bak -E "s|^([[:space:]]+local[[:space:]]+tia_up[[:space:]]+=[[:space:]]+resty_roundrobin:new).*$|\1\(\{"$IPS_TIA"\}\)|" /etc/nginx/conf.d/default.conf
+else
+    curl -s -H "Content-Type: application/json" -X POST -d "$(generate_post_data "The string OSMO is empty")" $discord_url
+fi
+
+if [ -n "$IPS_DYM" ]; then
+    sed -i.bak -E "s|^([[:space:]]+local[[:space:]]+dym_up[[:space:]]+=[[:space:]]+resty_roundrobin:new).*$|\1\(\{"$IPS_DYM"\}\)|" /etc/nginx/conf.d/default.conf
+else
+    curl -s -H "Content-Type: application/json" -X POST -d "$(generate_post_data "The string ATOM is empty")" $discord_url
+fi
+
+if [ -n "$IPS_NOBLE" ]; then
+    sed -i.bak -E "s|^([[:space:]]+local[[:space:]]+noble_up[[:space:]]+=[[:space:]]+resty_roundrobin:new).*$|\1\(\{"$IPS_NOBLE"\}\)|" /etc/nginx/conf.d/default.conf
+else
+    curl -s -H "Content-Type: application/json" -X POST -d "$(generate_post_data "The string INJ is empty")" $discord_url
+fi
 nginx -s reload
