68afdc3855b43f6cab75abcb3b5fc656ff1203c5
phutx <phu.tx@orai.io>
2024-11-19 17:44:00 +0700

    remove unuse line code

--- lib/resty/roundrobin.lua
+++ lib/resty/roundrobin.lua
@@ -7,8 +7,6 @@ local error = error
 local cjson = require "cjson"
 local utils = require "resty.balancer.utils"
 
-local read_config = utils.read_config
-local read_file = utils.read_file
 local copy = utils.copy
 local nkeys = utils.nkeys
 
@@ -75,17 +73,14 @@ local function get_random_node_id(nodes)
 end
 
 
-function _M.new(_, filePath)
-    local nodes = read_config(filePath)
+function _M.new(_, nodes)
     local newnodes = copy(nodes)
     -- by default height is weight
     local heights = copy(nodes)
-    local response_time = copy(nodes)
     local only_key, gcd, max_weight = get_gcd(newnodes)
     local last_id = get_random_node_id(nodes)
 
     local self = {
-        response_time = response_time, -- ip => block_height
         heights = heights, -- ip => block_height
         nodes = newnodes, -- it's safer to copy one
         only_key = only_key,
@@ -175,23 +170,19 @@ function _M.set(self, id, w)
     return _decr(self, id, old_weight - new_weight)
 end
 
-local function update(self, port)
+local function update(self)
     local httpc = require("resty.http").new()
     -- timeout 5 seconds for reading
     httpc:set_timeouts(2000, 2000, 5000)
     for id, _ in next, self.heights do
         -- query block height to update the heights
-        local start_time = ngx.now()
-        local res, _ = httpc:request_uri('http://' .. id .. ':' .. port .. '/abci_info', {
+        local res, _ = httpc:request_uri('http://' .. id .. ':26657/abci_info', {
                 method = "GET",
             })
-        local end_time = ngx.now()
-        local response_time = end_time - start_time
         if res then
             local new_block_height = get_block_height(res.body)
             if new_block_height > 0 then
                 self.heights[id] = new_block_height
-                self.response_time[id] = math.floor(response_time * 1000 + 0.5)
             end
         end
     end
